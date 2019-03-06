a9fff82d2d6402b38bf3641a22d71d1789f2eb15
djdajing <dj.dajing@hotmail.com>
2019-03-06 22:14:43 +0800

    bugfix for image preprocessing

--- preprocess_images/dataSplitter.py
+++ preprocess_images/dataSplitter.py
@@ -49,7 +49,6 @@ def copy_images(column_series, in_dir, out_dir):
 def split(in_dir,out_dir,csv):
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
-
     csv_helper = CsvHelper()
     csv_helper.set_csv(csv)
     image_paths_df=csv_helper.get_single_column(IMAGE_PATH)
--- preprocess_images/run.sh
+++ preprocess_images/run.sh
@@ -1,3 +1,3 @@
-python main.py -i /home/dj/NDSC/new_data/mobile/mobile_image/ -o /home/dj/NDSC/new_data/mobile/ -c mobile -d /home/dj/NDSC/csvs/
+python main.py -i /home/dj/NDSC/new_data/mobile2/mobile_image/ -o /home/dj/NDSC/new_data/mobile2/ -c mobile -d /home/dj/NDSC/csvs/
 
 #python main.py -i /home/dj/NDSC/new_data/beauty/beauty_image/ -o /home/dj/NDSC/new_data/beauty/ -c beauty -d /home/dj/NDSC/csvs/
